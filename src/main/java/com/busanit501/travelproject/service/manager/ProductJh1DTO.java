b4b034a050d134b0a0cf944542c62f294c571878
pvpanime <nemisprint@gmail.com>
2024-12-24 16:51:58 +0900

    Service 단위테스트중

---
+++ src/main/java/com/busanit501/travelproject/service/manager/ProductJh1DTO.java
@@ -0,0 +1,27 @@
+package com.busanit501.travelproject.service.manager;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductJh1DTO {
+  private Long productNo;
+  private String name;
+  private String description;
+  private Long price;
+  private Long locationNo;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  private int capacity;
+  private String imagePath;
+  private List<Object> reservations;
+  private List<Object> reviews;
+}
