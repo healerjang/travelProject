e3a0537cc6809fe1da234836357643af51f6f227
pvpanime <nemisprint@gmail.com>
2024-12-27 17:01:30 +0900

    2024-12-27 업데이트 테마 색을 개조한 /css/customize/bootstrap.css 적용 여행상품 목록에서 클릭 시 수정 화면으로 이동 (기능 미구현) 여행지 관리 화면

--- src/main/java/com/busanit501/travelproject/controller/admin/AdminJh1Controller.java
+++ src/main/java/com/busanit501/travelproject/controller/admin/AdminJh1Controller.java
@@ -1,13 +1,29 @@
 package com.busanit501.travelproject.controller.admin;
 
+import com.busanit501.travelproject.dto.ProductJh1DTO;
+import com.busanit501.travelproject.service.admin.AdminJh1Service;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminJh1Controller {
+
+  private final AdminJh1Service adminService;
 
   @GetMapping("/admin/product/register")
   public String registerProduct() {
+    return "admin/registerProduct_jh1";
+  }
+
+  @GetMapping("/admin/product/edit/{productNo}")
+  public String editProduct(@PathVariable("productNo") @ModelAttribute("productNo") Long productNo, Model model) {
+    ProductJh1DTO productDTO = adminService.getProductTmp(productNo);
+    model.addAttribute("productDTO", productDTO);
     return "admin/registerProduct_jh1";
   }
 
@@ -15,4 +31,7 @@ public class AdminJh1Controller {
   public String listProduct() {
     return "admin/listProduct_jh1";
   }
+
+  @GetMapping("/admin/location/list")
+  public String listLocation() { return "admin/listLocation_jh1"; }
 }
