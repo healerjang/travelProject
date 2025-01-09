4adac6063c0ee9ec271a5bea37efff92d6e7968e
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-09 10:13:28 +0900

    250109 cascade 삭제 주석

--- src/main/java/com/busanit501/travelproject/domain/FreeBoard.java
+++ src/main/java/com/busanit501/travelproject/domain/FreeBoard.java
@@ -25,6 +25,10 @@ public class FreeBoard extends BaseEntity{
     @JoinColumn(name = "member_no", nullable = false)
     private Member member;
     @OneToMany(mappedBy = "freeBoard", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    // cascade - sql 부모요소에게 수정,삭제 등 영향을 주면 자식 요소도 전부 받게 만들게 해야
+    // 외래키 설정이 되어있어도 삭제가 된다.
+    // 댓글 입장에서는 many to one 이지만
+    // 우린 지금 게시판 로직을 만들고 있으니 게시판 입장에서 one to many  가 됨
     private Set<Reply> replies;
 
     public void changeTitleContent(@NotEmpty @Size(min = 3, max = 100) String title, @NotEmpty String content) {
