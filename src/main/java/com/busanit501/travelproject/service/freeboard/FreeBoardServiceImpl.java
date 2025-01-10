7e1b6feb429ed5b4041205729625e6e8e334c287
alex6 <alex9305@naver.com>
2025-01-10 09:59:16 +0900

    member 빌더 추가 수정 및 주석 입력

--- src/main/java/com/busanit501/travelproject/service/freeboard/FreeBoardServiceImpl.java
+++ src/main/java/com/busanit501/travelproject/service/freeboard/FreeBoardServiceImpl.java
@@ -2,9 +2,9 @@ package com.busanit501.travelproject.service.freeboard;
 
 
 import com.busanit501.travelproject.domain.FreeBoard;
-import com.busanit501.travelproject.domain.Member;
 import com.busanit501.travelproject.dto.freeboard.*;
 import com.busanit501.travelproject.repository.freeboard.FreeBoardRepository;
+import com.busanit501.travelproject.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -27,18 +27,25 @@ public class FreeBoardServiceImpl implements com.busanit501.travelproject.servic
     private final FreeBoardRepository freeBoardRepository;
     private final ModelMapper modelMapper;
 
+    // memberRepository 추가
+    private final MemberRepository memberRepository;
+
     @Override
     public Long register(FreeBoardDTO freeBoardDTO) {
         FreeBoard freeBoard = FreeBoard.builder()
                 .title(freeBoardDTO.getTitle())
                 .content(freeBoardDTO.getContent())
-                .member(Member.builder().memberNo(freeBoardDTO.getMemberNo()).build())
+                .member(memberRepository.findById(freeBoardDTO.getMemberNo()).orElse(null))
                 .build();
                 //modelMapper.map(freeBoardDTO, FreeBoard.class);
         // builder. << 객체를 만들기 위한거임
         // 그래서 FreeBoard << 엔티티 클래스를 들고와서 빌드업 패턴으로 객체로 만들어줌
         // 왜냐하면 다른 title content 는 전부 문자열로 들어오기 때문에 형태가 같음
         // 그치만 member 는 memberNo라는 long 으로 받아야 하는데 객체로 되어 있으니까
+        
+        // 주석 추가 - 기존의 .member(member.Builder.memberNo(freeBoardDTO.getMemberNo()).build)의 방식은 불안정.
+        // member가 엔티티 클래스 이기에 jpa에서 데이터 베이스에 있는 memberNo가 있을 경우 로직 상으로는 큰 문제는 없으나 만약 데이터 베이스 내 memberNo가 없을 경우 SQLSyntaxErrorException이 날 거임.
+        // 그렇기에 member는 memberRepository로 하여금 Optional로 객체를 가져와 예외 처리를 해주는 게 안전
         Long freeBoardNo = freeBoardRepository.save(freeBoard).getFreeBoardNo();
         return freeBoardNo;
     }
